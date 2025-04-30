5f15fccdaf53f256f6e0d9ff70c4fce937fe1d39
abdullahshafaqat <abdullahshafaqat14@gmail.com>
2025-04-30 17:47:03 +0500

    adding more functionality

--- main.go
+++ main.go
@@ -3,33 +3,118 @@ package main
 import (
 	"fmt"
 	"os"
-
 )
 
-func readFile()string{
+var words int
+var lines int
+var spaces int
+var punc int
+var character int
+var senten int
+var vowels int
+var consonance int
+
+func wordCounter(filedata string) {
+	words = 0
+
+	for i := 0; i < len(filedata); i++ {
+
+		if filedata[i] == ' ' || filedata[i] == '.' || filedata[i] == ',' {
+			words++
+		}
+	}
+	fmt.Println("Number of words :", words)
+}
+
+func lineCounter(filedata string) {
+	lines = 1
+	for i := 0; i < len(filedata); i++ {
+		if filedata[i] == '\n' {
+			lines++
+		}
+	}
+	fmt.Println("Numbers of lines", lines)
+}
+
+func spaceCounter(filedata string) {
+	spaces = 0
+	for i := 0; i < len(filedata); i++ {
+		if filedata[i] == ' ' {
+			spaces++
+		}
+	}
+	fmt.Println("Numbers of spaces :", spaces)
+}
+
+func puncCounter(filedata string) {
+	punc = 0
+	for i := 0; i < len(filedata); i++ {
+		if filedata[i] == ',' || filedata[i] == '.' {
+			punc++
+		}
+	}
+	fmt.Println("Numbers of punctuations", punc)
+}
+
+func speciCounter(filedata string) {
+	character = 0
+	for i := 0; i < len(filedata); i++ {
+		if filedata[i] == '@' || filedata[i] == '$' || filedata[i] == '#' || filedata[i] == '%' || filedata[i] == '!' {
+			character++
+		}
+	}
+	fmt.Println("Numbers of special characters :", character)
+}
+func sentenceCounter(filedata string) {
+	senten = 0
+	for i := 0; i < len(filedata); i++ {
+		if filedata[i] == '.' {
+			senten++
+		}
+	}
+	fmt.Println("Numbers of sentences :", senten)
+}
+
+func vowelCounter(filedata string) {
+	vowels = 0
+	for i := 0; i < len(filedata); i++ {
+		if filedata[i] == 'a' || filedata[i] == 'e' || filedata[i] == 'i' || filedata[i] == 'o' || filedata[i] == 'u' {
+			vowels++
+		}
+	}
+	fmt.Println("Numbers of vowels :", vowels)
+}
+
+func consonCounnter(filedata string) {
+
+	consonance = 0
+	for i := 0; i < len(filedata); i++ {
+		switch filedata[i] {
+		case 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z',
+			'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z':
+			{
+				consonance++
+			}
+		}
+	}
+	fmt.Println("Numbers of consonance :", consonance)
+}
+
+func main() {
 	fmt.Println("Reading file")
 	filename := "Test.txt"
-	filedata ,err := os.ReadFile(filename)
+	filedata, err := os.ReadFile(filename)
 	if err != nil {
 		panic(err)
 	}
-	return string(filedata)
+	Str := string(filedata)
+
+	wordCounter(Str)
+	lineCounter(Str)
+	spaceCounter(Str)
+	puncCounter(Str)
+	speciCounter(Str)
+	sentenceCounter(Str)
+	vowelCounter(Str)
+	consonCounnter(Str)
 }
-
-func wordCounter(filedata string){
-	count := 0
-	for _, v := range filedata {
-		if v == ' '{
-			count++
-		}
-	}
-	fmt.Println("Number of words :", count)
-
-}
-
-
-func main() {
-	filedata := readFile()
-	wordCounter(filedata)
-}
-
