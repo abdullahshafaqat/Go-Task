6bb51d3b0ebd53bc30efcd561727f4f48bf52b21
abdullah-dev <abdullahshafaqat14@gmail.com>
2025-05-07 17:02:22 +0500

    Add files via upload
    creating api

--- chunks/chunks.go
+++ chunks/chunks.go
@@ -1,30 +1,53 @@
 package chunks
 
 import (
-	"GOTASKS/combine"
-	"fmt"
+
+	"github.com/abdullahshafaqat/GOTASKS/combine"
 )
+type names struct {
+      chunknumber int
+	  Words int
+	  Lines int
+	  Spaces int 
+	  Punctuation int
+	  Specialcharacters int
+	  Sentences int
+	  Vowels int
+	  Consonants int
+	  Digits int
+	  Paragraphs int
 
-func Chunks(fileData string) {
+}
+
+func Chunks(fileData string) []names{
 	chunkSize := len(fileData) / 4
-
-	var names = [10]string{"Words", "lines", "Spaces", "Punctuations", "Special Characters",
-		"Sentences", "Vowels", "Consonants", "Digits", "Paragraphs"}
 	chunk := []string{
 		fileData[:chunkSize],
 		fileData[chunkSize : chunkSize*2],
 		fileData[chunkSize*2 : chunkSize*3],
 		fileData[chunkSize*3:],
 	}
-
+ var chunkresult [] names
 	for i := 0; i < 4; i++ {
 		resultchann := make(chan []int)
 		go combine.AnalyzeText(chunk[i], resultchann)
-		fmt.Println("Chunk No", i)
 		value := <-resultchann
-		for j := 0; j < 10; j++ {
-			fmt.Println(names[j], value[j])
-		}
-	}
+        chunkresult = append(chunkresult, names{
+chunknumber: i+1,
+Words: value[0],
+Lines: value[1],
+Spaces: value[2],
+Punctuation: value[3],
+Specialcharacters: value[4],
+Sentences: value[5],
+Vowels: value[6],
+Consonants: value[7],
+Digits: value[8],
+Paragraphs: value[9],
 
+		})
+		
+		
+	}
+return chunkresult
 }
--- main.go
+++ main.go
@@ -1,57 +1,88 @@
 package main
 
 import (
-	"GOTASKS/chunks"
-	"GOTASKS/combine"
-	"GOTASKS/separate"
 	"fmt"
-	"os"
-	"sync"
+	"io"
+	"net/http"
 	"time"
+
+	"github.com/abdullahshafaqat/GOTASKS/chunks"
+	"github.com/abdullahshafaqat/GOTASKS/combine"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	startTime := time.Now()
-	var wg sync.WaitGroup
-	wg.Add(10)
 	fmt.Println("Reading file")
-	filename := "Test.txt"
-	fileData, err := os.ReadFile(filename)
-	if err != nil {
-		panic(err)
-	}
+	router := gin.Default()
+	router.POST("/analyzer", func(c *gin.Context) {
+		file, err := c.FormFile("file")
+		if err != nil {
+			c.String(http.StatusBadRequest, "error reading file: %v", err)
+			return
+		}
+		Data, _ := file.Open()
+		defer Data.Close()
+		fileData, _ := io.ReadAll(Data)
+		Str := string(fileData)
 
-	Str := string(fileData)
-	go separate.WordCounter(Str, &wg)
-	go separate.LineCounter(Str, &wg)
-	go separate.SpaceCounter(Str, &wg)
-	go separate.PuncCounter(Str, &wg)
-	go separate.SpeciCounter(Str, &wg)
-	go separate.SentenceCounter(Str, &wg)
-	go separate.VowelCounter(Str, &wg)
-	go separate.ConsonCounnter(Str, &wg)
-	go separate.DigitCounter(Str, &wg)
-	go separate.ParaCounter(Str, &wg)
-	wg.Wait()
-	timeTaken := time.Since(startTime)
-	fmt.Println("Time Taken After Using Goroutine: ", timeTaken)
+		// var wg sync.WaitGroup
+		// wg.Add(10)
+		// go separate.WordCounter(Str, &wg)
+		// go separate.LineCounter(Str, &wg)
+		// go separate.SpaceCounter(Str, &wg)
+		// go separate.PuncCounter(Str, &wg)
+		// go separate.SpeciCounter(Str, &wg)
+		// go separate.SentenceCounter(Str, &wg)
+		// go separate.VowelCounter(Str, &wg)
+		// go separate.ConsonCounnter(Str, &wg)
+		// go separate.DigitCounter(Str, &wg)
+		// go separate.ParaCounter(Str, &wg)
+		// wg.Wait()
+		timeTaken := time.Since(startTime)
+		resultchann := make(chan []int)
+		go combine.AnalyzeText(Str, resultchann)
+		value := <-resultchann
+		// c.JSON(200,gin.H{
+		//    "Filename": file.Filename,
+		//    "Using combine function Words": value[0],
+		//    "Using combine function Lines": value[1],
+		//    "Using combine function Spaces": value[2],
+		//    "Using combine function Punctuation": value[3],
+		//    "Using combine function Characters": value[4],
+		//    "Using combine function Sentences" : value[5],
+		//    "Using combine function Vowels": value[6],
+		//    "Using combine function Consonants" : value[7],
+		//    "Using combine function Digits": value[8],
+		//    "Using combine function Paragraphs" : value[9],
+		// })
+		// timeTaken := time.Since(startTime)
+		// fmt.Println("Time Taken After Using Channels & Goroutines: ", timeTaken)
+		// for i := 0; i < 10; i++ {
+		// 	fmt.Println(names[i], value[i])
+		// }
+		timeStart3 := time.Now()
 
-	startTime2 := time.Now()
-	resultchann := make(chan []int)
-	go combine.AnalyzeText(Str, resultchann)
-	var names = [10]string{"Words", "lines", "Spaces", "Punctuations", "Special Characters", "Sentences", "Vowels", "Consonants", "Digits", "Paragraphs"}
-	value := <-resultchann
-	for i := 0; i < 10; i++ {
-		fmt.Println(names[i], value[i])
-	}
+		result := chunks.Chunks(Str)
+		timeTaken3 := time.Since(timeStart3)
+		c.JSON(200, gin.H{
+			"Filename":                           file.Filename,
+			"Time Taken for chunks":              timeTaken3.String(),
+			"Result":                             result,
+			"Using combine function Words":       value[0],
+			"Using combine function Lines":       value[1],
+			"Using combine function Spaces":      value[2],
+			"Using combine function Punctuation": value[3],
+			"Using combine function Characters":  value[4],
+			"Using combine function Sentences":   value[5],
+			"Using combine function Vowels":      value[6],
+			"Using combine function Consonants":  value[7],
+			"Using combine function Digits":      value[8],
+			"Using combine function Paragraphs":  value[9],
+			"time taken for combine function":    timeTaken.String(),
+		})
 
-	timeTaken2 := time.Since(startTime2)
-	fmt.Println("Time Taken After Using Channels & Goroutines: ", timeTaken2)
-
-	timeStart3 := time.Now()
-	chunks.Chunks(Str)
-	timeTaken3 := time.Since(timeStart3)
-
-	fmt.Println("Time taken after making chunks :", timeTaken3)
+	})
+	router.Run(":8080")
 
 }
