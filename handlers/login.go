522f5ed8297cee0c9c8a324104ad8ad7a0214a43
abdullahshafaqat <abdullahshafaqat14.com>
2025-05-13 17:29:41 +0500

    update login handler

--- handlers/login.go
+++ handlers/login.go
@@ -7,32 +7,31 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
+var login struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
 func LogIn(db *sqlx.DB) gin.HandlerFunc {
-		return func(c *gin.Context) {
+	return func(c *gin.Context) {
 
-		var login struct{
-
-		Email string    `json:"email"`
-		Password string  `json:"password"`
-		}
-		if err := c.ShouldBindJSON(&login); err != nil{
-		c.JSON(http.StatusBadRequest,gin.H{"error": "Data not in json"})
-		return
+		if err := c.ShouldBindJSON(&login); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Data not in json"})
+			return
 		}
 		var user Info
 
-		err := db.Get(&user,`SELECT username,email,password
+		err := db.Get(&user, `SELECT username,email,password
 			FROM users
 			 WHERE email = $1 AND  password = $2`,
-			login.Email,login.Password)
-
+			login.Email, login.Password)
 
 		if err != nil {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid email or password"})
-		return
-		}			
-		c.JSON(http.StatusOK,gin.H{
-		"message" : "Login successfully",
-     	})
-   	}
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid email or password"})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Login successfully",
+		})
+	}
 }
